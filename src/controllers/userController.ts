65e2a7bf8c1420a7c70b8d47ec4f52d2f80254c9
miguelrp23 <angelromeromiguel2317@gmail.com>
2024-10-14 10:38:25 +0200

    update test

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+export const prisma = new PrismaClient();
 
 export const createUser = async (req: Request, res: Response) => {
     const { name } = req.body;
@@ -12,9 +12,11 @@ export const createUser = async (req: Request, res: Response) => {
                 name,
             },
         });
+
         res.status(201).json(user);
     } catch (error) {
         res.status(500).json({ error: 'Error creating user' });
+        
     }
 };
 
--- src/server.ts
+++ src/server.ts
@@ -14,3 +14,6 @@ app.use(errorHandler);
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
+
+export default app
+
